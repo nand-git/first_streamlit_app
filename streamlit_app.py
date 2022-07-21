59c6077aed586bee86447af5e0c32e7c9517da9c
Nandiprasad Gali <88129146+nand-git@users.noreply.github.com>
2022-07-21 09:17:12 +0530

    fruit picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as sl
 import pandas as pd
 
 sl.title("Breakfast Menu")
+sl.header("Fruit bowl")
 
 fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+fruit_list = fruit_list(set_index('Fruit'))
+sl.multiselect("Pick some fruits: ", list(fruit_list.index))
 sl.dataframe(fruit_list)
