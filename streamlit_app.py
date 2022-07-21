4c7efe300e5966f2152022c32a873f29a04557ee
Nandiprasad Gali <88129146+nand-git@users.noreply.github.com>
2022-07-21 09:18:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ sl.title("Breakfast Menu")
 sl.header("Fruit bowl")
 
 fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-fruit_list = fruit_list(set_index('Fruit'))
+fruit_list = fruit_list.set_index('Fruit')
 sl.multiselect("Pick some fruits: ", list(fruit_list.index))
 sl.dataframe(fruit_list)
