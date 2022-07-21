dbf9657b600377e7c01e18d1c462061dfe11ea75
Nandiprasad Gali <88129146+nand-git@users.noreply.github.com>
2022-07-21 09:24:44 +0530

    fruit to show

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,6 @@ sl.header("Fruit bowl")
 
 fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruit_list = fruit_list.set_index('Fruit')
-sl.multiselect("Pick some fruits: ", list(fruit_list.index),['Banana','Apple'])
-sl.dataframe(fruit_list)
+fruit_selected = sl.multiselect("Pick some fruits: ", list(fruit_list.index),['Banana','Apple'])
+fruit_to_show = fruit_list.loc[fruit_selected]
+sl.dataframe(fruit_to_show)
