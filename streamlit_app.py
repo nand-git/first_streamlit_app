1764fccaeb645be48c174c4cc1f2c72884fedd6a
Nandiprasad Gali <88129146+nand-git@users.noreply.github.com>
2022-08-05 17:44:00 +0530

    function

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,15 +14,19 @@ fruit_to_show = fruit_list.loc[fruit_selected]
 sl.dataframe(fruit_to_show)
 
 sl.header("Fruit Advice")
+def get_fuit_data(fruit_name):
+  fruit_res=req.get("https://fruityvice.com/api/fruit/" + fruit_name)
+  #sl.text(fruit_res.json())
+  fruit_norm = pd.json_normalize(fruit_res.json())
+  return fruit_norm
+
 try:
   fruit_choice=sl.text_input("Choose fruit: ")
   if not fruit_choice:
     sl.error("Please choose a fruit!")
   else:
-    fruit_res=req.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    #sl.text(fruit_res.json())
-    fruit_norm = pd.json_normalize(fruit_res.json())
-    sl.dataframe(fruit_norm)
+    fun_res=get_fuit_data(fruit_choice)
+    sl.dataframe(fun_res)
 except URLError as e:
   sl.error()
 
