adc1752f4687f4e9e5dda11ada4b383aee7d1bb9
Nandiprasad Gali <88129146+nand-git@users.noreply.github.com>
2022-08-05 15:40:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as sl
 import requests as req
 import pandas as pd
-import snowflake.connector as conn
+import snowflake.connector
 
 sl.title("Breakfast Menu")
 sl.header("Fruit bowl")
